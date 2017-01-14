054f112417483bbfd1db70f877b149a3a9882531
StevenSawtelle <steven.sawtelle@gmail.com>
2017-01-14 14:07:37 -0700

    changing location request messages and updating mobile app

--- healthkit/HeroinHelper/ViewController.swift
+++ healthkit/HeroinHelper/ViewController.swift
@@ -33,30 +33,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let long = long2.stringValue
         let lat = lat2.stringValue
         mainLabel.text = long
-        var request = URLRequest(url: URL(string: "http://172.20.10.4:5000/location")!)
-        request.httpMethod = "POST"
-        let postString = lat + ", " + long
-        request.httpBody = postString.data(using: .utf8)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {                                                 // check for fundamental networking error
-                print("error=\(error)")
-                return
-            }
-            
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(response)")
-            }
-            
-            let responseString = String(data: data, encoding: .utf8)
-            print("responseString = \(responseString)")
-        }
-        task.resume()
+        
+        makeRequest(message: (lat + ", " + long))
         
         let heartRateType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
-        
+        var csvString = ""
         if (HKHealthStore.isHealthDataAvailable()){
-            var csvString = "Time,Date,Heartrate(BPM)\n"
             self.healthStore.requestAuthorization(toShare: nil, read:[heartRateType], completion:{(success, error) in
                 let sortByTime = NSSortDescriptor(key:HKSampleSortIdentifierEndDate, ascending:false)
                 let timeFormatter = DateFormatter()
@@ -65,32 +47,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "MM/dd/YYYY"
                 
-                let query = HKSampleQuery(sampleType:heartRateType, predicate:nil, limit:600, sortDescriptors:[sortByTime], resultsHandler:{(query, results, error) in
+                let query = HKSampleQuery(sampleType:heartRateType, predicate:nil, limit:1, sortDescriptors:[sortByTime], resultsHandler:{(query, results, error) in
                     guard let results = results else { return }
                     for quantitySample in results {
                         let quantity = (quantitySample as! HKQuantitySample).quantity
                         let heartRateUnit = HKUnit(from: "count/min")
-                        
-                        //                        csvString.extend("\(quantity.doubleValueForUnit(heartRateUnit)),\(timeFormatter.stringFromDate(quantitySample.startDate)),\(dateFormatter.stringFromDate(quantitySample.startDate))\n")
-                        //                        println("\(quantity.doubleValueForUnit(heartRateUnit)),\(timeFormatter.stringFromDate(quantitySample.startDate)),\(dateFormatter.stringFromDate(quantitySample.startDate))")
-                        csvString += "\(timeFormatter.string(from: quantitySample.startDate)),\(dateFormatter.string(from: quantitySample.startDate)),\(quantity.doubleValue(for: heartRateUnit))\n"
-                        print("\(timeFormatter.string(from: quantitySample.startDate)),\(dateFormatter.string(from: quantitySample.startDate)),\(quantity.doubleValue(for: heartRateUnit))")
+                        csvString = "\(timeFormatter.string(from: quantitySample.startDate)), \(dateFormatter.string(from: quantitySample.startDate)), \(quantity.doubleValue(for: heartRateUnit))\n"
+                        self.makeRequest(message: csvString)
                     }
-                    
-                    do {
-                        //let documentsDir = try FileManager.defaultManager.URLForDirectory(.DocumentDirectory, inDomain:.UserDomainMask, appropriateForURL:nil, create:true)
-                        //try csvString.writeToURL(NSURL(string:"heartratedata.csv", relativeToURL:documentsDir)!, atomically:true, encoding:String.Encoding.ascii)
-                        print(csvString)
-                    }
-                    catch {
-                        print("Error occured")
-                    }
-                    
                 })
                 self.healthStore.execute(query)
             })
  
         }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -108,6 +78,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         location = locations[0]
     }
-
+    
+    func makeRequest(message: String){
+        var request = URLRequest(url: URL(string: "http://172.20.10.4:5000/location")!)
+        request.httpMethod = "POST"
+        let postString = message
+        print(postString)
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
 }
 
