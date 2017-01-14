a090bd62cea99eccc2096e9c14eab14039d28eb5
Eric-Arellano <ericarellano@me.com>
2017-01-14 12:41:57 -0700

    deduplicate HTTP get requests

---
+++ server/request.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+def accept_location():
+    accept_request('/location')
+
+
+def accept_hr():
+    accept_request('/hr')
+
+
+def accept_friend_info():
+    accept_request('/friend-info')
+
+
+@app.route('<string:address>', methods=['GET', 'POST'])
+def accept_request(address):
+    content = request.form
+    print content
+    print content.split()
+    return 'hi'
