067f07181df5d68aee13ed3f61e68a7c734d053e
Eric-Arellano <ericarellano@me.com>
2017-01-14 14:21:45 -0700

    add twilio text support

--- server/response.py
+++ server/response.py
@@ -1,12 +1,34 @@
-from flask import Flask
-import twilio.twiml
+from twilio.rest import TwilioRestClient
 import os  # for API Keys
 
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-
-app = Flask(__name__)
+client = TwilioRestClient(account_sid, auth_token)
 
 
 def trigger_response():
-    pass
+    from_number = get_from_number()
+    to_number = get_to_number()
+    message = get_message()
+    send_text(message, to_number, from_number)
+
+
+def send_text(from_, to, message):
+    message = client.messages.create(body=message,
+                                     to=to,
+                                     from_=from_)
+    print(message.sid)
+
+
+def get_from_number():
+    return "+15203917018"
+
+
+def get_to_number():
+    return "+19258585614"
+
+
+def get_message():
+    return "SOS I NEED HELP ABHIK"
+
+trigger_response()
--- server/twilio-credentials_example.sh
+++ server/twilio-credentials_example.sh
@@ -1,2 +1,4 @@
 export TWILIO_ACCOUNT_SID="example"
 export TWILIO_AUTH_TOKEN="example"
+
+echo "Twilio credentials set."
