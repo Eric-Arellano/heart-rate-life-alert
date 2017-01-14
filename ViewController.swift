3e6fc677b78345486b534ce88c3eb7a75be823bf
StevenSawtelle <steven.sawtelle@gmail.com>
2017-01-14 10:58:24 -0700

    initial mobile commit

---
+++ ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  HeroinHelper
+//
+//  Created by Steven Sawtelle on 1/14/17.
+//  Copyright © 2017 Steven Sawtelle. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import HealthKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locManager = CLLocationManager()
+    let healthStore = HKHealthStore()
+    
+    @IBAction func hourSlider(_ sender: UISlider) {
+        sliderLabel.text = String(format: "%.2f", sender.value)
+    }
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBAction func shootUp(_ sender: UIButton) {
+        mainLabel.text = "Test"
+        let currentLocation = locManager.location
+        let long2 = NSNumber(value: currentLocation!.coordinate.longitude)
+        let lat2 = NSNumber(value: currentLocation!.coordinate.latitude)
+        let long = long2.stringValue
+        let lat = lat2.stringValue
+        mainLabel.text = long
+        var request = URLRequest(url: URL(string: "http://172.20.10.4:5000/location")!)
+        request.httpMethod = "POST"
+        let postString = lat + ", " + long
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+        
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+        
+        if (HKHealthStore.isHealthDataAvailable()){
+            var csvString = "Time,Date,Heartrate(BPM)\n"
+            self.healthStore.requestAuthorization(toShare: nil, read:[heartRateType], completion:{(success, error) in
+                let sortByTime = NSSortDescriptor(key:HKSampleSortIdentifierEndDate, ascending:false)
+                let timeFormatter = DateFormatter()
+                timeFormatter.dateFormat = "hh:mm:ss"
+                
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "MM/dd/YYYY"
+                
+                let query = HKSampleQuery(sampleType:heartRateType, predicate:nil, limit:600, sortDescriptors:[sortByTime], resultsHandler:{(query, results, error) in
+                    guard let results = results else { return }
+                    for quantitySample in results {
+                        let quantity = (quantitySample as! HKQuantitySample).quantity
+                        let heartRateUnit = HKUnit(from: "count/min")
+                        
+                        //                        csvString.extend("\(quantity.doubleValueForUnit(heartRateUnit)),\(timeFormatter.stringFromDate(quantitySample.startDate)),\(dateFormatter.stringFromDate(quantitySample.startDate))\n")
+                        //                        println("\(quantity.doubleValueForUnit(heartRateUnit)),\(timeFormatter.stringFromDate(quantitySample.startDate)),\(dateFormatter.stringFromDate(quantitySample.startDate))")
+                        csvString += "\(timeFormatter.string(from: quantitySample.startDate)),\(dateFormatter.string(from: quantitySample.startDate)),\(quantity.doubleValue(for: heartRateUnit))\n"
+                        print("\(timeFormatter.string(from: quantitySample.startDate)),\(dateFormatter.string(from: quantitySample.startDate)),\(quantity.doubleValue(for: heartRateUnit))")
+                    }
+                    
+                    do {
+                        //let documentsDir = try FileManager.defaultManager.URLForDirectory(.DocumentDirectory, inDomain:.UserDomainMask, appropriateForURL:nil, create:true)
+                        //try csvString.writeToURL(NSURL(string:"heartratedata.csv", relativeToURL:documentsDir)!, atomically:true, encoding:String.Encoding.ascii)
+                        print(csvString)
+                    }
+                    catch {
+                        print("Error occured")
+                    }
+                    
+                })
+                self.healthStore.execute(query)
+            })
+        }
+
+        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
+
